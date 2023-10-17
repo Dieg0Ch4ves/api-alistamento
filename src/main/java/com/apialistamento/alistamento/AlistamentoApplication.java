0aa97b3067516b2b609ebc16267704cecc56f2e6
Dieg0Ch4ves <diegoaraujo200570@gmail.com>
2023-10-17 09:46:04 -0400

    bugfix CORS

--- src/main/java/com/apialistamento/alistamento/AlistamentoApplication.java
+++ src/main/java/com/apialistamento/alistamento/AlistamentoApplication.java
@@ -2,11 +2,13 @@ package com.apialistamento.alistamento;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API de alistamento"))
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API de alistamento"),
+		servers = { @Server(url = "/" , description = "Default Server URL")})
 public class AlistamentoApplication {
 
 	public static void main(String[] args) {
